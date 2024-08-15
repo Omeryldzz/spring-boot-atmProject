ec2c2659e1fff518dd5ddf89b340813db28e4071
ömer yıldız <yildizom20@itu.edu.tr>
2024-08-15 12:15:29 +0300

    security disabled bugs fixed

--- backend/src/main/java/com/omery/projectAtm/ProjectAtmApplication.java
+++ backend/src/main/java/com/omery/projectAtm/ProjectAtmApplication.java
@@ -2,8 +2,10 @@ package com.omery.projectAtm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//  exclude = {SecurityAutoConfiguration.class} disables security
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ProjectAtmApplication {
 
 	public static void main(String[] args) {
